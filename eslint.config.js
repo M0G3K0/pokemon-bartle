ff868cf7c5152f5cd6ff6dc251ce63f2d1e3a723
{M0G3K0} <{xx.M0G3K0.xx@gmail.com}>
2025-12-12 02:40:48 +0900

    fix(lint): update ESLint config for design system
    - Change component/directive prefix from 'app' to 'pb'
    - Allow both element and attribute selectors for components
    - Disable elements-content rule for directive-based components
    - Disable interactive-supports-focus for host-binding components

--- eslint.config.js
+++ eslint.config.js
@@ -19,15 +19,15 @@ module.exports = defineConfig([
         "error",
         {
           type: "attribute",
-          prefix: "app",
+          prefix: "pb",
           style: "camelCase",
         },
       ],
       "@angular-eslint/component-selector": [
         "error",
         {
-          type: "element",
-          prefix: "app",
+          type: ["element", "attribute"],
+          prefix: "pb",
           style: "kebab-case",
         },
       ],
@@ -39,6 +39,11 @@ module.exports = defineConfig([
       angular.configs.templateRecommended,
       angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      // ディレクティブベースのコンポーネントがコンテンツを動的に生成するため無効化
+      "@angular-eslint/template/elements-content": "off",
+      // ディレクティブベースのコンポーネントがhostバインディングでclickを使用するため無効化
+      "@angular-eslint/template/interactive-supports-focus": "off",
+    },
   }
 ]);
